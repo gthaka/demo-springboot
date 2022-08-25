0d84ba1c147a01c9aa2e6dfdce95ff7cb86eabc5
Anthony G <agithaka@live.com>
2022-08-26 01:55:05 +0300

    refactor: Student - define primary key - auto increment Id column

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,13 @@
 package com.example.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity // for hibernate
+@Table// table in db
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
