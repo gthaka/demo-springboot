a46fb52dadccad80160dd801f7114df396aeff7f
Anthony G <agithaka@live.com>
2022-08-26 00:44:09 +0300

    refactor:Student class - define attribs

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,12 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+}
