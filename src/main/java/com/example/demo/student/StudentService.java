b133531ecf2c61ea1d0174f383afb6fd8b27ec0e
Anthony G <agithaka@live.com>
2022-08-26 02:48:35 +0300

    refactor: StudentService - registration method

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -18,4 +18,8 @@ public class StudentService {
     public List<Student> getStudents() {
         return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+    }
 }
