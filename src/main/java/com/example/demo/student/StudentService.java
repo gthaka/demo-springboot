1ee438b48d1487f29f0063a2ee29f03129833f30
Anthony G <agithaka@live.com>
2022-08-26 02:12:39 +0300

    refactor: StudentService - getStudents from studentRepo

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,21 +1,21 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L,
-                        "Paul",
-                        "paul@example.com",
-                        LocalDate.of(2000, Month.DECEMBER, 5),
-                        21)
-        );
+        return studentRepository.findAll();
     }
 }
