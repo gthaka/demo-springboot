83e10bf5f6ca2ed9c10a47a57db04c98f6fb251b
Anthony G <agithaka@live.com>
2022-08-26 00:54:25 +0300

    refactor:Rest Demo App - hello return list of student

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,32 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
-	}
+    @GetMapping
+    public List<Student> hello() {
+        return List.of(
+                new Student(1L,
+                        "Paul",
+                        "paul@example.com",
+                        LocalDate.of(2000, Month.DECEMBER, 5),
+                        21)
+        );
+    }
 
 }
