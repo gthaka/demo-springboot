822a1086eeab709c134e807ee4caaacfa88fa4b6
Anthony G <agithaka@live.com>
2022-08-26 01:24:20 +0300

    refactor: StudentService - specify class as Service. similar to component

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,12 @@
 package com.example.demo.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
 
     public List<Student> getStudents() {
