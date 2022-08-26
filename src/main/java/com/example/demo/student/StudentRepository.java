3cdbd54150f4cf643c17259ce19eb44050dd4d2b
Anthony G <agithaka@live.com>
2022-08-26 03:04:06 +0300

    refactor: StudentRepository - unused import

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
