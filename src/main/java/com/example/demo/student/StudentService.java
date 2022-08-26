67cbe4f8fcffab1b7f3ecb08b81807c525817e10
Anthony G <agithaka@live.com>
2022-08-26 03:38:55 +0300

    refactor: StudentService - add student update service as transactional - pre-validate name & email -save info on success

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,7 +3,9 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,5 +36,23 @@ public class StudentService {
             throw new IllegalStateException("Student with id "+studentId+" does not exit");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional // Entity goes in a managed state
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()->new IllegalStateException("Student with id "+studentId+" does not exit"));
+
+        if(name != null && name.length() > 0 && !Objects.equals(student.getName(),name)) {
+            student.setName(name);
+        }
+
+        if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(),email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
